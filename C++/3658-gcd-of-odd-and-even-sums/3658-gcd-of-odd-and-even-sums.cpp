5016c5fa2a0776137b3cf339b02fe5b8a820b5a6
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-07-15 22:52:54 +0530

    Time: 579 ms (5.15%), Space: 8.7 MB (9.11%) - LeetHub

--- C++/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ C++/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+    int god(int sumEven,int sumOdd){
+        int x=min(sumEven,sumOdd);
+        int ans=INT_MIN;
+        for(int i=1;i<x+1;i++){
+            if(sumEven%i==0 && sumOdd%i==0){
+                ans=max(ans,i);
+            }
+        }
+        return ans;
+    }
     int gcdOfOddEvenSums(int n) {
-        return n;
+        if(n==1) return n;
+        int sumOdd=n*n;
+        int sumEven=n*(n-1);
+        return god(sumEven,sumOdd);
     }
 };
