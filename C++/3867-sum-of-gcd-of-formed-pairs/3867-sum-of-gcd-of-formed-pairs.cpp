4674bac09458ef830b672684af7b672cf73bbeb4
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-07-16 23:33:31 +0530

    Time: 92 ms (9.06%), Space: 155.8 MB (62.98%) - LeetHub

--- C++/3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ C++/3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    /*long long gcd(long long minn,long long maxx){
-        int x=minn;
-        while(x!=0){
-            if(minn%x==0 && maxx%x==0) return x;
-            else
-            x--;
+    long long gcd(long long a,long long b){
+        while(b){
+            a%=b;
+            swap(a,b);
         }
-        return 0;
-    }*/
+        return a;
+    }
 
     long long gcdSum(vector<int>& nums) {
         int n=nums.size();
@@ -18,13 +16,13 @@ public:
 
         for(int i=0;i<n;i++){
             m=max(m,nums[i]);
-            prefixGod[i]=std::gcd(nums[i],m);            
+            prefixGod[i]=gcd(nums[i],m);            
         }
         sort(prefixGod.begin(),prefixGod.end());
         long long sum=0;
         int l=0,r=n-1;
         while(l<r){
-            sum+=std::gcd(prefixGod[l],prefixGod[r]);
+            sum+=gcd(prefixGod[l],prefixGod[r]);
             r--;
             l++;
         }
