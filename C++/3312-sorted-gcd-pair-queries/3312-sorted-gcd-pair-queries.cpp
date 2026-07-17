7a1c7ace5aa993278283cd208bb7763d279f45c5
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-07-18 01:12:27 +0530

    Time: 24 ms (98.8%), Space: 134.2 MB (21.69%) - LeetHub

--- C++/3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ C++/3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -2,26 +2,46 @@ class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
         int mx=*max_element(nums.begin(),nums.end());
-        vector<int> freq(mx+1,0);
-        vector<long long> GCD(mx+1,0);
+        vector<long long>freq(mx+1,0);
+        for(int x:nums)freq[x]++;
 
-        for(auto& it:nums) freq[it]++;
-        
-        for(int i=mx;i>0;i--){
-            long long sum=0,extra=0;
-            for(int j=i;j<=mx;j+=i){
-                sum+=freq[j];
-                extra+=GCD[j];
+        vector<long long> multiples(mx+1,0);
+        for(int g=1;g<=mx;g++){
+            for(int k=g;k<=mx;k+=g){
+                multiples[g]+=freq[k];
             }
-            GCD[i]=sum*(sum-1)/2-extra;
         }
-        partial_sum(GCD.begin(),GCD.end(),GCD.begin());
 
-        vector<int> ans(queries.size());
-        for(int i=0;i<queries.size();i++){
-            ans[i]=ranges::upper_bound(GCD,queries[i])-GCD.begin();
+        vector<long long> exact(mx+1,0);
+        for(int g=mx;g>=1;g--){
+            long long c=multiples[g];
+            long long total=c*(c-1)/2;
+            for(int k=2*g;k<=mx;k+=g){
+                total-=exact[k];
+            }
+            exact[g]=total;
         }
-        
+
+        //prefix sum
+        vector<long long> pref(mx+1,0);
+        for(int g=1;g<=mx;g++) pref[g]=pref[g-1]+exact[g];  
+
+        //binary search
+        vector<int> ans;
+        ans.reserve(queries.size());
+        for(long long it:queries){
+            int l=1,h=mx,res=-1;
+            while(l<=h){
+                int mid=(l+h)/2;
+                if(pref[mid]>it){
+                    res=mid;
+                    h=mid-1;
+                }
+                else
+                l=mid+1;
+            }
+            ans.push_back(res);
+        }
         return ans;
     }
 };
