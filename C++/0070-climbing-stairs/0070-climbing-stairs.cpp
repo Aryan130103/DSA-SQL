af7a4869ac204af3b38e188ecd7569f51c4f3d32
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-07-27 02:20:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.53%) - LeetHub

--- C++/0070-climbing-stairs/0070-climbing-stairs.cpp
+++ C++/0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int climbStairs(int n){
         if(n==0 || n==1) return 1;
-        int p1=1,p2=1,temp=0;
+        int curr=1,prev=1;
         for(int i=2;i<=n;i++){
-            temp=p1+p2;
-            p2=p1;
-            p1=temp;
+            curr=curr+prev;
+            prev=curr-prev;
         }
-        return p1;
+        return curr;
     }
 };
