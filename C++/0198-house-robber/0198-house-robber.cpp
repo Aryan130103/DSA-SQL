65718fc0a1a1ba3b4dd6e40b1ee6b3f28c8beb57
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-07-30 02:27:00 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.93%) - LeetHub

--- C++/0198-house-robber/0198-house-robber.cpp
+++ C++/0198-house-robber/0198-house-robber.cpp
@@ -1,4 +1,5 @@
 class Solution {
+ /*MEMOISATION   
 private:
     int f(int i,vector<int>& nums,vector<int>& dp){
         if(i==0) return nums[0];
@@ -14,5 +15,38 @@ public:
         int n=nums.size();
         vector<int> dp(n,-1);
         return f(n-1,nums,dp);
+    }*/
+
+//TABULATION
+/*
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int notake=dp[i-1];
+            dp[i]=max(take,notake);
+        }
+    return dp[n-1];
+    }*/
+
+//SPACE OPTIMISATION
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev1=nums[0];
+        int prev2=0,curr=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int notake=prev1;
+            curr=max(take,notake);
+            prev2=prev1;
+            prev1=curr;
+        }
+    return prev1;
     }
 };
