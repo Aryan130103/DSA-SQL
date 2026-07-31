d6a541a1a1172a8a76d22f271f395b43d90d51cb
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-01 01:41:53 +0530

    Time: 0 ms (100%), Space: 11.7 MB (79.22%) - LeetHub

---
+++ C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>& dp){
+        if(i==0 && j==0) return grid[0][0]==0;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(grid[i][j]==1) return 0;
+        
+        int up=grid[i][j]+ f(i-1,j,grid,dp);
+        int left=grid[i][j]+ f(i,j-1,grid,dp);
+        
+        return dp[i][j]=up+left; 
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,grid,dp);
+    }
+};
