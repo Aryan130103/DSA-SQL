1826b27e3527adb1fe71af83de5d11b066868247
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-01 02:04:53 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.81%) - LeetHub

--- C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -12,7 +12,7 @@ private:
         
         return dp[i][j]=up+left; 
     }*/
-//tabulation
+/*tabulation
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n=grid.size();
@@ -36,5 +36,35 @@ public:
             }
         }
     return dp[n-1][m-1];
+    }
+*/
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<int>prev(m,0);
+        
+        if(grid[0][0]==1) return 0;
+        prev[0]=1;
+
+        for(int i=0;i<n;i++){
+            vector<int> curr(m,0);
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    curr[j]=0;
+                    continue;
+                } 
+                if(i==0 && j==0) {
+                    curr[j]=1;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0) up=prev[j];
+                if(j>0) left=curr[j-1];
+                curr[j]=up+left;
+            }
+            prev=curr;
+        }
+    return prev[m-1];
     }
 };
