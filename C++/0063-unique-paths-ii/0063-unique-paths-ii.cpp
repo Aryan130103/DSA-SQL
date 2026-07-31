f99394fa53bc2057cd658c362fcdff4ebe9f8ef8
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-01 01:56:20 +0530

    Time: 0 ms (100%), Space: 11.8 MB (55.36%) - LeetHub

--- C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {/*memoisation
 private:
     int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>& dp){
         if(i==0 && j==0) return grid[0][0]==0;
@@ -11,12 +11,30 @@ private:
         int left=f(i,j-1,grid,dp);
         
         return dp[i][j]=up+left; 
-    }
+    }*/
+//tabulation
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,grid,dp);
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        if(grid[0][0]==1) return 0;;
+        dp[0][0]=1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                } 
+                if(i==0&& j==0) continue;
+                int up=0,left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+    return dp[n-1][m-1];
     }
 };
