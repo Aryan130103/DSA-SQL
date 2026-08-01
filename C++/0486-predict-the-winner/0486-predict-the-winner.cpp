e417f2d38149eea665ce72b3b9ea10a44c76fcf2
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-01 20:57:35 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

---
+++ C++/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int f(int l,int r,vector<int>& nums, vector<vector<int>>& dp){
+        if(l==r) return nums[l];
+        if(dp[l][r]!=-1) return dp[l][r];
+      
+        int left=nums[l]-f(l+1,r,nums,dp);
+        int right=nums[r]-f(l,r-1,nums,dp);
+
+        return dp[l][r]=max(left,right);
+    }
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return f(0,n-1,nums,dp)>=0;
+    }
+};
