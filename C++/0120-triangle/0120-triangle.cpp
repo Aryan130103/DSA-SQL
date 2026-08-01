3aba352295c1b8c4cc5ad1d6c5bc212f86bc03e8
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-02 01:23:51 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.58%) - LeetHub

--- C++/0120-triangle/0120-triangle.cpp
+++ C++/0120-triangle/0120-triangle.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {/*
 private:
     int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
         int n=triangle.size();
@@ -8,11 +8,21 @@ private:
         int d=triangle[i][j]+f(i+1,j,triangle,dp);
         int dg=triangle[i][j]+f(i+1,j+1,triangle,dp);
         return dp[i][j]=min(d,dg);
-    }
+    }*/
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,1e9));
-        return f(0,0,triangle,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++)
+            dp[n-1][j]=triangle[n-1][j];
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int d=triangle[i][j]+dp[i+1][j];
+                int dg=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
