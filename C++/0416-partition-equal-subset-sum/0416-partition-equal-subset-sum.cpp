985be618c3161c40931fba209edea720f6ce3db2
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-04 04:25:12 +0530

    Time: 151 ms (36.18%), Space: 17.4 MB (60.16%) - LeetHub

--- C++/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ C++/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {/*
 private:
     int f(int i,int s,vector<int>& nums,vector<vector<int>>& dp){
         if(s==0) return true;
@@ -11,7 +11,7 @@ private:
             take=f(i-1,s-nums[i],nums,dp);
 
         return dp[i][s]= take ||  notake;
-    }
+    }*/
 public:
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
@@ -19,7 +19,24 @@ public:
         for(auto i:nums) sum+=i;
         if(sum%2) return false; 
         int s=sum/2;
-        vector<vector<int>>dp(n,vector<int>(s+1,-1));
-        return f(n-1,s,nums,dp);
+        vector<vector<bool>>dp(n,vector<bool>(s+1,false));
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=s) dp[0][nums[0]]=true;
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=s;j++){
+                bool notake=dp[i-1][j];
+                bool take=false;
+                if(nums[i]<=j)
+                    take=dp[i-1][j-nums[i]];
+
+                dp[i][j]= take ||  notake;
+            }
+        }
+        
+        return dp[n-1][s];
     }
 };
