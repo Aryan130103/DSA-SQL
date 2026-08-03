ac4e501401f5e101e0d1de868e212b93d4bc5801
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-04 04:07:42 +0530

    Time: 131 ms (55.57%), Space: 110.5 MB (40.52%) - LeetHub

---
+++ C++/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int f(int i,int s,vector<int>& nums,vector<vector<int>>& dp){
+        if(s==0) return true;
+        if(dp[i][s]!=-1) return dp[i][s];
+        if(i==0) return (nums[i]==s);
+
+        bool notake=f(i-1,s,nums,dp);
+        bool take=false;
+        if(nums[i]<=s)
+            take=f(i-1,s-nums[i],nums,dp);
+
+        return dp[i][s]= take ||  notake;
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(auto i:nums) sum+=i;
+        if(sum%2) return false; 
+        int s=sum/2;
+        vector<vector<int>>dp(n,vector<int>(s+1,-1));
+        return f(n-1,s,nums,dp);
+    }
+};
