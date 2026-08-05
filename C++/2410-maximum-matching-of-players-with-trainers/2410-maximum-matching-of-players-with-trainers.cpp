61558fb1b407c392568c896ff258d5b01f50990f
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-05 20:15:42 +0530

    Time: 30 ms (45.45%), Space: 80.2 MB (80.95%) - LeetHub

---
+++ C++/2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int n=players.size();
+        int m=trainers.size();
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(trainers[j]>=players[i]){
+                j++;
+                i++;
+            }
+            else
+            j++;
+        }
+        return i;
+    }
+};
