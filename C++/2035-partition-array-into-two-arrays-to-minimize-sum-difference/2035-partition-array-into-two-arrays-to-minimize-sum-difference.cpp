ffc939195c52352c18b6ddf820b0225257f9f7f8
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-06 14:53:02 +0530

    Time: 371 ms (76.69%), Space: 73.4 MB (56.59%) - LeetHub

--- C++/2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ C++/2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int minimumDifference(vector<int>& nums) {
-        int n=nums.size();
+    int minimumDifference(vector<int>& arr) {
         int sum=0;
-        for(auto i:nums) sum+=i;
-        int s=n/2;
-        vector<vector<int>> left(s+1),right(s+1);
+        int n=arr.size()/2;
+        for(auto i:arr) sum+=i;
+        vector<vector<int>>left(n+1),right(n+1);
 
-        for(int mask=0;mask<(1<<s);mask++){
+        for(int mask=0;mask<(1<<n);mask++){
             int lsum=0,rsum=0,cnt=0;
-            for(int i=0;i<s;i++){
+            for(int i=0;i<n;i++){
                 if(mask&(1<<i)){
                     cnt++;
-                    lsum+=nums[i];
-                    rsum+=nums[i+s];
+                    lsum+=arr[i];
+                    rsum+=arr[i+n];                    
                 }
             }
             left[cnt].push_back(lsum);
             right[cnt].push_back(rsum);
         }
 
-        for(int i=0;i<=s;i++){
+        int ans=INT_MAX;
+        for(int i=0;i<=n;i++){
             sort(right[i].begin(),right[i].end());
         }
 
-        int ans=INT_MAX;
-        for(int k=0;k<=s;k++){
-            auto &vec=right[s-k];
-            for(int lsum:left[k]){
+        for(int i=0;i<=n;i++){
+            auto& vec=right[n-i];
+            for(auto lsum:left[i]){
                 int target=sum/2-lsum;
-            
                 auto it=lower_bound(vec.begin(),vec.end(),target);
 
                 if(it!=vec.end()){
@@ -38,12 +36,12 @@ public:
                 }
 
                 if(it!=vec.begin()){
-                it--;
-                int s1=*it+lsum;
-                ans=min(ans,abs(sum-2*s1));
+                    it--;
+                    int s1=*it+lsum;
+                    ans=min(ans,abs(sum-2*s1));
                 }
             }
         }
-        return ans;
+    return ans;
     }
 };
