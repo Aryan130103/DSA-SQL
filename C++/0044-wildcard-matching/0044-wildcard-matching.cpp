72f6b687d2db7aa9143b8e03338466fee7969ee1
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-14 01:51:54 +0530

    Time: 51 ms (54.78%), Space: 12.8 MB (79.76%) - LeetHub

--- C++/0044-wildcard-matching/0044-wildcard-matching.cpp
+++ C++/0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -4,8 +4,8 @@ private:/*
         if(i==0 && j==0) return true;
         if(i>0 && j==0) return false;
         if(i==0 && j>0){
-            for(int x=0;x<j;x++){
-                if(p[x]!='*') return false;
+            for(int x=1;x<j;x++){
+                if(p[x-1]!='*') return false;
             }
             return true;
         }
@@ -22,22 +22,25 @@ private:/*
 public:
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));{
-            dp[0][0]=true;
+        vector<bool>prev(m+1,false);
+        prev[0]=true;
+
+        for(int j=1;j<=m;j++){
+            if(p[j-1]=='*') prev[j]=prev[j-1];
+        }    
+                
+
+        for(int i=1;i<=n;i++){
+            vector<bool>curr(m+1,false);
             for(int j=1;j<=m;j++){
-                if(p[j-1]=='*') dp[0][j]=dp[0][j-1];
-            }  
 
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=m;j++){
-
-                    if(s[i-1]==p[j-1] || p[j-1]=='?') 
-                        dp[i][j]=dp[i-1][j-1];
-                    if(p[j-1]=='*')
-                        dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                }
+                if(s[i-1]==p[j-1] || p[j-1]=='?') 
+                    curr[j]=prev[j-1];
+                if(p[j-1]=='*')
+                    curr[j]=prev[j] || curr[j-1];
             }
+            prev=curr;
         }
-        return dp[n][m];
+    return prev[m];
     }
 };
