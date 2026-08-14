0b39c4596158b4de154fab014c291ac1664442d9
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-14 11:40:23 +0530

    Time: 9 ms (37.6%), Space: 13.2 MB (43.54%) - LeetHub

--- C++/0072-edit-distance/0072-edit-distance.cpp
+++ C++/0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int minDistance(string s1, string s2) {
-        int n=s1.size(),m=s2.size();
+    int minDistance(string s, string t) {
+        int n=s.size(),m=t.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++)   dp[i][0]=i;
-        for(int j=1;j<=m;j++)   dp[0][j]=j;
+
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1])
+                if(s[i-1]==t[j-1])
                     dp[i][j]=dp[i-1][j-1];
                 else{
-                    int insert=dp[i][j-1];
                     int del=dp[i-1][j];
                     int replace=dp[i-1][j-1];
-                    dp[i][j]=1+min({insert,del,replace});
-                }
+                    int insert=dp[i][j-1];
+                    dp[i][j]=1+min({del,insert,replace});
+                }                    
             }
         }
         return dp[n][m];
