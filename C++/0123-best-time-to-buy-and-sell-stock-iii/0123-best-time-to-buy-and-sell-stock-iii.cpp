55dd3a66b28eeee136dd3521350819ae62a430f2
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-15 15:06:13 +0530

    Time: 135 ms (61.84%), Space: 139.6 MB (59.02%) - LeetHub

--- C++/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ C++/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,46 +1,20 @@
-class Solution {/*
-private:
-    int f(int i,int buy,int cap,vector<int>& p,vector<vector<vector<int>>>& dp){
-        if(cap==0) return 0;
-        if(i==p.size()) return 0;
-        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-
-        int profit=0;
-        if(buy){
-            profit=max((-p[i]+f(i+1,0,cap,p,dp)),f(i+1,1,cap,p,dp));
-        }
-        else{
-            profit=max((p[i]+f(i+1,1,cap-1,p,dp)),f(i+1,0,cap,p,dp));
-        }
-        return dp[i][buy][cap]=profit;
-    }*/
+class Solution {
 public:
     int maxProfit(vector<int>& p) {
         int n=p.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        /*
-        for(int i=n;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                dp[i][buy][0]=0;
-            }
-        }
-        dp[n][0][1]=dp[n][0][2]=0;
-        dp[n][1][1]=dp[n][1][2]=0;*/
-
+        vector<vector<int>>dp(n+1,(vector<int>(5,0)));
+        int profit=0;
         for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<3;cap++){
-                    int profit=0;
-                    if(buy){
-                        profit=max(-p[i]+dp[i+1][0][cap], dp[i+1][1][cap]);
-                    }
-                    else{
-                        profit=max(p[i]+dp[i+1][1][cap-1], dp[i+1][0][cap]);
-                    }
-                    dp[i][buy][cap]=profit;
+            for(int t=0;t<=3;t++){
+                if(t%2==0){
+                    profit=max(-p[i]+dp[i+1][t+1],dp[i+1][t]);
                 }
+                else{
+                    profit=max(p[i]+dp[i+1][t+1],dp[i+1][t]);
+                }
+                dp[i][t]=profit;
             }
         }
-    return dp[0][1][2];
+        return dp[0][0];
     }
 };
