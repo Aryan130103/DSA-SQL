15b0f302f4590f02a44d46698af6451a35f71110
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-16 02:20:12 +0530

    Time: 0 ms (100%), Space: 16.1 MB (64.81%) - LeetHub

--- C++/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ C++/0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,16 +4,9 @@ public:
         int n=p.size();
         vector<vector<int>>dp(n+2,vector<int>(2,0));
         for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                int profit=0;
-                if(buy){
-                    profit=max(-p[i]+dp[i+1][0],dp[i+1][1]);
-                }
-                else{
-                    profit=max(p[i]+dp[i+2][1],dp[i+1][0]);
-                }
-                dp[i][buy]=profit;
-            }
+            int profit=0;
+            dp[i][1]=max(-p[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][0]=max(p[i]+dp[i+2][1],dp[i+1][0]);
         }
         return dp[0][1];
     }
