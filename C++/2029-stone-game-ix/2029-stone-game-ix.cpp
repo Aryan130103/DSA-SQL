e94a1486d987e6567bf78cf4fe7ef48a204cc5c3
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-16 18:13:33 +0530

    Time: 11 ms (16.75%), Space: 131.3 MB (33.5%) - LeetHub

---
+++ C++/2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int c0=0,c1=0,c2=0;
+        for(auto i:stones){
+            if(i%3==0) c0++;
+            else if(i%3==1) c1++;
+            else c2++;
+        }
+        if(c1==0 && c2==0) return false;
+
+        if(c0%2==0)
+            return c1>0 && c2>0;
+        
+        return abs(c1-c2)>2;
+    }
+};
