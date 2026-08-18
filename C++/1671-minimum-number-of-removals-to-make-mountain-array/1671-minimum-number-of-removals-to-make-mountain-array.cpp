da81af90dc9d50c05fb656579fd017781e79d9ba
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-18 12:35:18 +0530

    Time: 55 ms (69.42%), Space: 16.2 MB (52.62%) - LeetHub

--- C++/1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ C++/1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -3,13 +3,13 @@ public:
     int minimumMountainRemovals(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,1),dp1(n,1);
-        int maxx=0,maxx1=0;
+        //int maxx=0,maxx1=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j] && dp[i]<dp[j]+1)
                     dp[i]=dp[j]+1;
             }
-            maxx=max(maxx,dp[i]);
+           //maxx=max(maxx,dp[i]);
         }
 
         
@@ -18,7 +18,7 @@ public:
                 if(nums[i]>nums[j] && dp1[i]<dp1[j]+1)
                     dp1[i]=dp1[j]+1;
             }
-            maxx1=max(maxx1,dp1[i]);
+            //maxx1=max(maxx1,dp1[i]);
         }
 
         int ans=0;
