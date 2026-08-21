e449931d2f1d1383d066b3df84b760e77e26916f
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-21 16:33:12 +0530

    Time: 93 ms (82.51%), Space: 13.8 MB (84%) - LeetHub

--- C++/0312-burst-balloons/0312-burst-balloons.cpp
+++ C++/0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {/*
 private:
     int f(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
         if(i>j) return 0;
@@ -9,13 +9,25 @@ private:
             maxx=max(maxx,cost);
         }
         return dp[i][j]=maxx;
-    }
+    }*/
 public:
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(1,n-2,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int i=n-2;i>=1;i--){
+            for(int j=i;j<=n-2;j++){
+                int maxx=0;
+                for(int k=i;k<=j;k++){
+                    int cost=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    maxx=max(maxx,cost);
+                }
+                dp[i][j]=maxx;
+            }
+        }
+
+        return dp[1][n-2];
     }
 };
