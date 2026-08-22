aa750b20a5e960077f04dacba9f88b2afc521c17
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-22 15:57:30 +0530

    Time: 22 ms (39.54%), Space: 23.5 MB (92.17%) - LeetHub

--- C++/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ C++/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int,pair<int,int>>>q;
         int n=grid.size();
         int m=grid[0].size();
-        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
-        if(n==1 && m==1) return 1;
         vector<vector<int>>dist(n,vector<int>(m,1e9));
-        dist[0][0]=1;
-        q.push({1,{0,0}});
-        while(!q.empty()){
-            int dis=q.front().first;
-            int r=q.front().second.first;
-            int c=q.front().second.second;
-            q.pop();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
 
-            int dr[]={-1,-1,-1,0,1,1,1,0};
-            int dc[]={-1,0,1,1,1,0,-1,-1};
+        dist[0][0]=1;
+        pq.push({1,{0,0}});
+
+        int dr[]={-1,-1,-1,0,1,1,1,0};
+        int dc[]={-1,0,1,1,1,0,-1,-1};
+
+        while(!pq.empty()){
+            auto it=pq.top();
+            int dst=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
+            pq.pop();
+
+            if(r==n-1 && c==m-1) return dst;
+
             for(int i=0;i<8;i++){
-                int nrow=r+dr[i];
-                int ncol=c+dc[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && dis+1<dist[nrow][ncol]){
-                    dist[nrow][ncol]=dis+1;
-                    if(nrow==n-1 && ncol==m-1) return dis+1;
-                    q.push({dis+1,{nrow,ncol}});
+                int nrow=dr[i]+r;
+                int ncol=dc[i]+c;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0){
+                    if(dist[nrow][ncol]>dst+1){
+                        dist[nrow][ncol]=dst+1;
+                        pq.push({dist[nrow][ncol],{nrow,ncol}});
+                    }
                 }
             }
         }
