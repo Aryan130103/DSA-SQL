664c7a0ccd87878a143fb1ce6a3ad8beb759bc1b
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-25 11:25:28 +0530

    Time: 11 ms (9.55%), Space: 25.2 MB (24.36%) - LeetHub

--- C++/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ C++/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        int maxx=0;
-        for(auto i:nums) m[i]++;
+        //unordered_map<int,int> m;
+        //int maxx=0;
+        //for(auto i:nums) m[i]++;
 
-        maxx=*max_element(nums.begin(),nums.end());
+        //maxx=*max_element(nums.begin(),nums.end());
 
-        int n=maxx+2*k;
-        int ans=0;
-        for(int i=1;i<n;i++){
-            if(m.find(k*i)==m.end()){
-                ans=k*i;
-                break;
+        //int n=maxx+2*k;
+        //int ans=0;
+        unordered_set<int> s(nums.begin(),nums.end());
+        for(int i=1;;i++){
+            if(s.find(k*i)==s.end()){
+                //ans=k*i;
+                //break;
+                return k*i;
             }
         }
-        return ans;
+        return 0;
     }
 };
