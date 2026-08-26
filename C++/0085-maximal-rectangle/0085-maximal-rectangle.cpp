d49d335f4ea3694b8631b29d0b3fb8102f938782
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-26 14:51:19 +0530

    Time: 3 ms (91.11%), Space: 19.3 MB (71.49%) - LeetHub

--- C++/0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ C++/0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,36 +1,37 @@
 class Solution {
-private:
-    int maxArea(vector<int>& heights) {
-        int n=heights.size();
-        int maxx=0;
-        stack<int> st;
-        for(int i=0;i<=n;i++){
-            while(!st.empty() && (i==n || heights[st.top()]>=heights[i])){
-               int h=heights[st.top()];
-                st.pop();
-                int w;
-                if(st.empty())w=i;
-                else 
-                    w=i-st.top()-1;
-                maxx=max(maxx,h*w);
-            }
-            st.push(i);
-        }  
-        return maxx;
-    }
 public:
-    int maximalRectangle(vector<vector<char>>& grid) {
+int largestRectangleArea(vector<int>& h) {
+        int n=h.size();
+        stack<int> st;
         int ans=0;
-        int n=grid.size(),m=grid[0].size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || h[st.top()]>h[i])) {
+                int l=h[st.top()];
+                st.pop();
+                int w=0;
+                if(st.empty()) w=i;
+                else
+                    w=i-st.top()-1;
+                ans=max(ans,l*w);
+            }
+            if(i<n)
+            st.push(i);
+        }
+        return ans;
+    }
+
+    int maximalRectangle(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
         vector<int> h(m,0);
+        int maxx=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1') h[j]++;
                 else h[j]=0;
-            }
-            int area=maxArea(h);
-            ans=max(ans,area);
-        }
-        return ans;
+            }   
+            maxx=max(maxx,largestRectangleArea(h));
+        } 
+        return maxx;
     }
 };
