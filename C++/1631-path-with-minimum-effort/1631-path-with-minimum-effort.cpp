3526209ff476bdf3842f642a68bcdd0c393ff456
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-08-28 21:14:37 +0530

    Time: 65 ms (36.26%), Space: 24.5 MB (56.36%) - LeetHub

--- C++/1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ C++/1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size();
-        int m=heights[0].size();
-
-        //pq={diff,{row,col}}
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int n=h.size();
+        int m=h[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        vector<vector<int>>dist (n,vector<int>(m,1e9));
 
-        pq.push({0,{0,0}});
-        dist[0][0]=0;
         int dr[]={-1,0,1,0};
         int dc[]={0,1,0,-1};
 
+        dist[0][0]=0;
+        pq.push({0,{0,0}});
         while(!pq.empty()){
-            int diff=pq.top().first;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
+            auto it=pq.top();
+            int effort=it.first;
+            int r=it.second.first;
+            int c=it.second.second;
             pq.pop();
 
-            if(r==n-1 && c==m-1) return diff;
+            if(r==n-1 && c==m-1) return effort;
 
             for(int i=0;i<4;i++){
                 int nrow=dr[i]+r;
                 int ncol=dc[i]+c;
-
+               
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
-                    int newEffort=max(abs(heights[nrow][ncol]-heights[r][c]),diff);
-                    if(newEffort < dist[nrow][ncol]){
-                        dist[nrow][ncol]=newEffort;
-                        pq.push({newEffort,{nrow,ncol}});
+                    int hdiff=abs(h[r][c]-h[nrow][ncol]);
+                    int maxx=max(effort,hdiff);
+
+                    if(maxx<dist[nrow][ncol]){
+                        dist[nrow][ncol]=maxx;
+                        pq.push({maxx,{nrow,ncol}});
                     }
                 }
             }
