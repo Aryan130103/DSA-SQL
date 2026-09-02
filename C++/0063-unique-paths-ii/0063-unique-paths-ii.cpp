548c00b0e2d728f95b9ee99a7b8c570905f830e2
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-02 16:15:59 +0530

    Time: 0 ms (100%), Space: 12.1 MB (6.61%) - LeetHub

--- C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ C++/0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,70 +1,22 @@
-class Solution {/*memoisation
+class Solution {
 private:
-    int f(int i,int j,vector<vector<int>>&grid,vector<vector<int>>& dp){
-        if(i==0 && j==0) return grid[0][0]==0;
+    int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i==0 && j==0) return grid[i][j]==0?1:0;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(grid[i][j]==1) return 0;
-        
-        int up=f(i-1,j,grid,dp);
-        int left=f(i,j-1,grid,dp);
-        
-        return dp[i][j]=up+left; 
-    }*/
-/*tabulation
+
+        int up=0,left=0;
+        if(grid[i][j]!=1){
+            up=f(i-1,j,grid,dp);
+            left=f(i,j-1,grid,dp);
+        }
+        return dp[i][j]=up+left;
+    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
-        
-        if(grid[0][0]==1) return 0;
-        dp[0][0]=1;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    dp[i][j]=0;
-                    continue;
-                } 
-                if(i==0&& j==0) continue;
-                int up=0,left=0;
-                if(i>0) up=dp[i-1][j];
-                if(j>0) left=dp[i][j-1];
-                dp[i][j]=up+left;
-            }
-        }
-    return dp[n-1][m-1];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,grid,dp);
     }
-*/
-public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<int>prev(m,0);
-        
-        if(grid[0][0]==1) return 0;
-        prev[0]=1;
-
-        for(int i=0;i<n;i++){
-            vector<int> curr(m,0);
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    curr[j]=0;
-                    continue;
-                } 
-                if(i==0 && j==0) {
-                    curr[j]=1;
-                    continue;
-                }
-                int up=0,left=0;
-                if(i>0) up=prev[j];
-                if(j>0) left=curr[j-1];
-                curr[j]=up+left;
-            }
-            prev=curr;
-        }
-    return prev[m-1];
-    }
-};
+}; 
