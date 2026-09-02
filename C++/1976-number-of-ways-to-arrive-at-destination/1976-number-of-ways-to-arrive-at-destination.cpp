71f7534f64d0f00287ead76c883c53626f181fe3
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-02 16:40:12 +0530

    Time: 8 ms (59.58%), Space: 35.2 MB (69.69%) - LeetHub

--- C++/1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ C++/1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n);
-        const int MOD=1000000007;
+        vector<vector<pair<int,int>>>adj(n);
         for(auto &it:roads){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
+        int MOD=1e9+7;
         priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
-        vector<long long> dist(n,LLONG_MAX);
         vector<int> ways(n,0);
-        dist[0]=0;
+        vector<long long> time(n,LLONG_MAX);
+        time[0]=0;
         ways[0]=1;
         pq.push({0,0});
 
         while(!pq.empty()){
             auto it=pq.top();
-            long long dst=it.first;
+            long long t=it.first;
             int node=it.second;
             pq.pop();
 
-            if(dst>dist[node]) continue;
+            if(t>time[node]) continue;
 
-            for(auto &it:adj[node]){
+            for(auto it:adj[node]){
                 int adjnode=it.first;
                 long long wt=it.second;
-                if(dst+wt<dist[adjnode]){
+
+                if(time[adjnode]>t+wt){
+                    time[adjnode]=t+wt;
                     ways[adjnode]=ways[node];
-                    dist[adjnode]=dst+wt;
-                    pq.push({dst+wt,adjnode});
+                    pq.push({t+wt,adjnode});
                 }
-                else if(dst+wt==dist[adjnode]){
-                    ways[adjnode]=(ways[adjnode]+ways[node])%MOD;
-                }
+                else if(time[adjnode]==t+wt)
+                     ways[adjnode]=(ways[adjnode]+ways[node])%MOD;
             }
         }
         return ways[n-1];
