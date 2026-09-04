fdba50d93a60968b4481dd4eacbc358779919ffa
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-04 16:50:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- C++/0120-triangle/0120-triangle.cpp
+++ C++/0120-triangle/0120-triangle.cpp
@@ -1,49 +1,17 @@
-class Solution {/*memoisation
+class Solution {
 private:
-    int f(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+    int f(int i,int j, vector<vector<int>>& triangle,vector<vector<int>>& dp){
         int n=triangle.size();
         if(i==n-1) return triangle[i][j];
-        if(dp[i][j]!=1e9) return dp[i][j];
-        
+        if(dp[i][j]!=1e9) return 1e9;
         int d=triangle[i][j]+f(i+1,j,triangle,dp);
         int dg=triangle[i][j]+f(i+1,j+1,triangle,dp);
         return dp[i][j]=min(d,dg);
-    }*/
-/*tabulation
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int j=0;j<n;j++)
-            dp[n-1][j]=triangle[n-1][j];
-
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int d=triangle[i][j]+dp[i+1][j];
-                int dg=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(d,dg);
-            }
-        }
-        return dp[0][0];
     }
-};*/
-
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int>front (n,0);
-        for(int j=0;j<n;j++)
-            front[j]=triangle[n-1][j];
-
-        for(int i=n-2;i>=0;i--){
-            vector<int>curr(n,0);
-            for(int j=i;j>=0;j--){
-                int d=triangle[i][j]+front[j];
-                int dg=triangle[i][j]+front[j+1];
-                curr[j]=min(d,dg);
-            }
-            front=curr;
-        }
-        return front[0];
+        vector<vector<int>>dp (n,vector<int>(n,1e9));
+        return f(0,0,triangle,dp);
     }
 };
