32c59df3e5d487a209a1de6f58f6511b61d92b90
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-10 02:45:55 +0530

    Time: 17 ms (54.02%), Space: 46.7 MB (42.66%) - LeetHub

--- C++/1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ C++/1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,51 +1,54 @@
-class disjointset{
+class disjoint{
+    vector<int>parent,size;
     public:
-    vector<int> size,parent;
-    disjointset(int n){
+    disjoint(int n){
         size.resize(n+1,1);
         parent.resize(n+1);
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<n;i++)
             parent[i]=i;
     }
 
-    int ult_p(int n){
-        if(n==parent[n]) return n;
-        return parent[n]=ult_p(parent[n]);
+    int ult_p(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=ult_p(parent[x]);
     }
+/*
+    bool find(int u,int v){
+        int UP=0,VP=0;
+        UP=ult_p(u);
+        VP=ult_p(v);
+        if(UP==VP) return true;
+        return false;
+    }*/
 
     void unionbysize(int u,int v){
-        int UP=ult_p(u);
-        int VP=ult_p(v);
+        int UP=0,VP=0;
+        UP=ult_p(u);
+        VP=ult_p(v);
         if(UP==VP) return;
-        
-        if(size[UP]<size[VP]){
-            parent[UP]=parent[VP];
-            size[VP]+=size[UP];
+        if(size[UP]>size[VP]){
+            parent[VP]=UP;
+            size[UP]+=size[VP];
         }
         else{
-            parent[VP]=parent[UP];
-            size[UP]+=size[VP];
+            parent[UP]=VP;
+            size[VP]+=size[UP];
         }
     }
 };
-
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1) return -1;
-
-        disjointset ds(n);
-        for(auto &it:connections){
-            int u=it[0];
-            int v=it[1];
-            ds.unionbysize(u,v);
+        disjoint d(n);
+        for(auto it:connections){
+            d.unionbysize(it[0],it[1]);
         }
 
         int NC=0;
         for(int i=0;i<n;i++){
-            if(ds.ult_p(i)==i) NC++;
+            if(d.ult_p(i)==i) NC++;
         }
-
         return NC-1;
     }
 };
