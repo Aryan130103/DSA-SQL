27a4f70c030d55d6f17ee91617d94d950e78febc
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-12 02:27:44 +0530

    Time: 0 ms (100%), Space: 28.7 MB (88.72%) - LeetHub

---
+++ C++/3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int totalNumbers(auto& digits) {
+        int f[10]={0};
+        int ans=0;
+
+        for(auto i:digits) f[i]++;
+
+        for(int i=1;i<10;i++){
+            for(int j=0;j<10;j++){
+                for(int k=0;k<9;k+=2){
+                    ans+=f[i]>0 && f[j]>(i==j) && f[k]>(i==k)+(j==k);
+                }
+            }
+        }
+        return ans;
+    }
+};
