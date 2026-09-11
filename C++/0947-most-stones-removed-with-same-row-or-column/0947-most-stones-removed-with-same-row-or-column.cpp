cf875ce73a1a7c6f951212298d6956500779ae35
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-11 13:38:37 +0530

    Time: 12 ms (79.46%), Space: 22.6 MB (46.72%) - LeetHub

--- C++/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ C++/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,7 +1,7 @@
-class disjointset{
-    public:
-    vector<int> size,parent;
-    disjointset(int n){
+class disjoint{
+public:
+    vector<int> parent,size;
+    disjoint(int n){
         size.resize(n+1,1);
         parent.resize(n+1);
         for(int i=0;i<=n;i++)
@@ -14,10 +14,10 @@ class disjointset{
     }
 
     void unionbysize(int u,int v){
-        int UP=ult_p(u);
-        int VP=ult_p(v);
+        int UP=0,VP=0;
+        UP=ult_p(u);
+        VP=ult_p(v);
         if(UP==VP) return;
-        
         if(size[UP]<size[VP]){
             parent[UP]=VP;
             size[VP]+=size[UP];
@@ -28,31 +28,31 @@ class disjointset{
         }
     }
 };
-
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        int maxrow=0,maxcol=0;
-        for(auto& it:stones){
-            maxrow=max(maxrow,it[0]);
-            maxcol=max(maxcol,it[1]);
+        int maxr=0,maxc=0;
+        for(auto &it:stones){
+            maxr=max(maxr,it[0]);
+            maxc=max(maxc,it[1]);
         }
-        disjointset ds(maxrow+maxcol+1);
-        unordered_map<int,int> stonenodes;
-        for(auto& it:stones){
-            int noderow=it[0];
-            int nodecol=it[1]+maxrow+1;
-            ds.unionbysize(noderow,nodecol);
-            stonenodes[noderow]=1;
-            stonenodes[nodecol]=1;
-        }
-       
-        int NC=0;
-        for(auto& it:stonenodes){
-            if(ds.ult_p(it.first)==it.first) NC++;
+        disjoint d(maxr+maxc+1);
+        unordered_map<int,int> m;
+
+        for(auto &it : stones){
+            int nrow=it[0];
+            int ncol=it[1]+maxr+1;
+            d.unionbysize(nrow,ncol);
+            m[nrow]=1;
+            m[ncol]=1;
         }
 
+        int NC=0;
+        for(auto& it:m){
+            if(it.first==d.ult_p(it.first))
+                NC++;
+        }
         return n-NC;
     }
 };
