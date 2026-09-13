0c79f5bb655ba2074a48fa7898e984f74d857988
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-13 16:17:08 +0530

    Time: 2 ms (57.65%), Space: 8.1 MB (5.31%) - LeetHub

--- C++/0278-first-bad-version/0278-first-bad-version.cpp
+++ C++/0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,15 +5,14 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l=1,r=n;
-        int minn=INT_MAX;
+        
         while(l<=r){
             int mid=l+(r-l)/2;
-            bool x=isBadVersion(mid);
-            if(x) minn=min(minn,mid);
-            if(!x) l=mid+1;
+            
+            if(isBadVersion(mid)) r=mid-1;
             else
-                r=mid-1;
+                l=mid+1;
         }
-        return minn;
+        return l;
     }
 };
