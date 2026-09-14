59a771efa08d0096c72707bd31e5400e391bd3bb
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-14 13:03:17 +0530

    Time: 13 ms (76.71%), Space: 22.6 MB (46.74%) - LeetHub

--- C++/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ C++/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,48 +1,52 @@
-class disjoint{
-public:
-    vector<int> parent,size;
-    disjoint(int n){
-        size.resize(n+1,1);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++)
-            parent[i]=i;
-    }
-
-    int ult_p(int n){
-        if(n==parent[n]) return n;
-        return parent[n]=ult_p(parent[n]);
-    }
-
-    void unionbysize(int u,int v){
-        int UP=0,VP=0;
-        UP=ult_p(u);
-        VP=ult_p(v);
-        if(UP==VP) return;
-        if(size[UP]<size[VP]){
-            parent[UP]=VP;
-            size[VP]+=size[UP];
+class Disjoint{
+    public:
+        vector<int> parent,size;
+        Disjoint(int n){
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++) parent[i]=i;
         }
-        else{
-            parent[VP]=UP;
-            size[UP]+=size[VP];
+
+        int ult_p(int n){
+            if(n==parent[n]) return n;
+            return parent[n]=ult_p(parent[n]);
         }
-    }
+
+        void unionbysize(int u,int v){
+            int UP=ult_p(u);
+            int VP=ult_p(v);
+
+            if(UP==VP) return ;
+
+            if(size[UP]<size[VP]){
+                parent[UP]=VP;
+                size[VP]+=size[UP];
+            }
+            else{
+                parent[VP]=UP;
+                size[UP]+=size[VP];
+            }
+        }
 };
+
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        int maxr=0,maxc=0;
-        for(auto &it:stones){
-            maxr=max(maxr,it[0]);
-            maxc=max(maxc,it[1]);
-        }
-        disjoint d(maxr+maxc+1);
-        unordered_map<int,int> m;
+        int maxxr=0,maxxc=0;
+        
 
-        for(auto &it : stones){
+        for(auto& it:stones){
+            if(it[0]>maxxr) maxxr=it[0];
+            if(it[1]>maxxc) maxxc=it[1];
+        }
+
+        unordered_map<int,int> m;
+        Disjoint d(maxxr+maxxc+1);
+        
+        for(auto& it:stones){
             int nrow=it[0];
-            int ncol=it[1]+maxr+1;
+            int ncol=it[1]+maxxr+1;
             d.unionbysize(nrow,ncol);
             m[nrow]=1;
             m[ncol]=1;
@@ -53,6 +57,7 @@ public:
             if(it.first==d.ult_p(it.first))
                 NC++;
         }
+
         return n-NC;
     }
 };
