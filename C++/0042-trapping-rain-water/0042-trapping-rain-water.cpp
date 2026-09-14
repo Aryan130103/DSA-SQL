69c7832a78b4ceceeaa7192ec51539ffb9b4c3ce
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-15 01:49:31 +0530

    Time: 0 ms (100%), Space: 26 MB (55.77%) - LeetHub

--- C++/0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ C++/0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,18 +4,17 @@ public:
         int n=height.size();
         int l=0,r=n-1,lmax=0,rmax=0;
         int rain=0;
+
         while(l<r){
-            if(height[l]<=height[r]){
-                lmax=max(lmax,height[l]);
-                rain+=lmax-height[l];
-                l++;
-            }
-            else{
-                rmax=max(rmax,height[r]);
-                rain+=rmax-height[r];
-                r--;
-            }
+            lmax=max(lmax,height[l]);
+            rmax=max(rmax,height[r]);
+
+            if(lmax<=rmax)    
+                rain+=lmax-height[l++];
+            else
+                rain+=rmax-height[r--];
         }
+
         return rain;
     }
 };
