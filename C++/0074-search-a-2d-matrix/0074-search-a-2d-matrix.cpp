7c25400b1fd793f7bd0571325a3c05d8b38ea7c8
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-15 22:16:13 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.29%) - LeetHub

--- C++/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ C++/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& nums, int target) {
-        int n=nums.size();
-        int m=nums[0].size();
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
         int l=0,r=n*m-1;
+
         while(l<=r){
             int mid=l+(r-l)/2;
-
             int row=mid/m;
             int col=mid%m;
 
-            if(nums[row][col]==target) return true;
-            if(nums[row][col]<target)
-                l=mid+1;
-            else 
-                r=mid-1;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]>target) r=mid-1;
+            else
+            l=mid+1;
         }
-        return false;   
+        return false;
     }
 };
