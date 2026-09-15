6d3105ee3f312be6bb8b9f7d140b7dd8fcf6f16f
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-15 21:52:06 +0530

    Time: 10 ms (47.06%), Space: 17.8 MB (58.33%) - LeetHub

--- C++/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ C++/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& p) {
-         int n=p.size();
-        vector<vector<int>>dp(n+1,(vector<int>(2*k+1,0)));
-        int profit=0;
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+
+       
         for(int i=n-1;i>=0;i--){
             for(int t=0;t<2*k;t++){
-                if(t%2==0){
-                    profit=max(-p[i]+dp[i+1][t+1],dp[i+1][t]);
-                }
-                else{
-                    profit=max(p[i]+dp[i+1][t+1],dp[i+1][t]);
-                }
+                 int profit;
+                if(t%2==0)
+                    profit=max(-prices[i]+dp[i+1][t+1],dp[i+1][t]);
+                else
+                    profit=max(prices[i]+dp[i+1][t+1],dp[i+1][t]);
+
                 dp[i][t]=profit;
             }
         }
