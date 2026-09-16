f7314623357dadc9095d5cabdd990f2aa4e6dedf
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-16 10:25:32 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.26%) - LeetHub

--- C++/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ C++/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,16 +5,13 @@ public:
         int m=matrix[0].size();
 
         int l=0,r=n*m-1;
-
         while(l<=r){
             int mid=l+(r-l)/2;
             int row=mid/m;
             int col=mid%m;
-
             if(matrix[row][col]==target) return true;
             else if(matrix[row][col]>target) r=mid-1;
-            else
-            l=mid+1;
+            else l=mid+1;
         }
         return false;
     }
