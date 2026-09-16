c0d6f383752d5bf908e1783ed088c6cf77888011
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-16 10:43:57 +0530

    Time: 10 ms (50.19%), Space: 11.6 MB (43.96%) - LeetHub

--- C++/0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ C++/0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,15 +3,16 @@ public:
     string minWindow(string s, string t) {
         int n=s.size(),m=t.size();
         if(n<m) return "";
+        unordered_map<char,int> mp;
+        
+        int len=INT_MAX,l=0,count=0,start=0;
 
-        unordered_map<char,int>mp;
-        for(auto& it:t) mp[it]++;
-
-        int l=0,len=INT_MAX,start=0,count=0;
+        for(auto& ch:t) mp[ch]++;
 
         for(int r=0;r<n;r++){
-            if(mp[s[r]]>0) count++;
-
+            if(mp[s[r]]>0){
+                count++;
+            }
             mp[s[r]]--;
 
             while(count==m){
@@ -24,7 +25,7 @@ public:
 
                 if(mp[s[l]]>0)
                     count--;
-                
+
                 l++;
             }
         }
