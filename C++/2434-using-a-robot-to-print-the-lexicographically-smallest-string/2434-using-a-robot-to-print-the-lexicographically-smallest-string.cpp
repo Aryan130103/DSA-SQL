a825876f7a7d15a5ecc4634acce9a3337a840fe5
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-17 14:49:02 +0530

    Time: 42 ms (75.17%), Space: 34.8 MB (58.7%) - LeetHub

--- C++/2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ C++/2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     string robotWithString(string s) {
         int n=s.size();
-        stack<char> t;
-        vector<char> mn(n);
-        string ans="";
-
+        stack<char>t;
+        vector<char>mn(n);
         mn[n-1]=s[n-1];
+        string ans;
+
         for(int i=n-2;i>=0;i--){
             mn[i]=min(s[i],mn[i+1]);
         }
 
         for(int i=0;i<n;i++){
             t.push(s[i]);
-
+            
             while(!t.empty() && (i==n-1 || t.top()<=mn[i+1])){
                 ans+=t.top();
                 t.pop();
