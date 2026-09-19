538826ee153b1de98df590ce0bbb303ef6b7afa8
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-19 15:53:00 +0530

    Time: 3 ms (9.67%), Space: 9.1 MB (71.54%) - LeetHub

--- C++/0062-unique-paths/0062-unique-paths.cpp
+++ C++/0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,13 @@
 class Solution {
 private:
     int f(int i,int j,vector<vector<int>>& dp){
+        if(i<0 || j<0) return 0;
         if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=0;
-        int left=0;
-        if(i>0) 
-            left=f(i-1,j,dp);
-        if(j>0)
-            up=f(i,j-1,dp);
-        int maxx=0;
+
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+
         return dp[i][j]=up+left;
     }
 public:
