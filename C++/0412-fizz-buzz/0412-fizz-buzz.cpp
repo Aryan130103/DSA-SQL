df27a53bad74f70aba01c4f7bbf0c5bd0380cd10
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-20 00:00:09 +0530

    Time: 1 ms (54.59%), Space: 11.1 MB (94.9%) - LeetHub

--- C++/0412-fizz-buzz/0412-fizz-buzz.cpp
+++ C++/0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,14 +3,12 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans(n);
         for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0)
-                ans[i-1]="FizzBuzz";
-            else if(i%3==0)
-                ans[i-1]="Fizz";
-            else if(i%5==0)
-                ans[i-1]="Buzz";
-            else 
-                ans[i-1]=to_string(i);
+            string s;
+            
+            if(i%3==0) s+="Fizz";
+            if(i%5==0) s+="Buzz";
+
+            ans[i-1]=(s.empty())?to_string(i):s;
         }
         return ans;
     }
