120749cab34b3afa25508c2721b5bf09c2219ba6
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-19 11:23:38 +0530

    Time: 46 ms (66.7%), Space: 29 MB (92.39%) - LeetHub

--- C++/0015-3sum/0015-3sum.cpp
+++ C++/0015-3sum/0015-3sum.cpp
@@ -4,23 +4,21 @@ public:
         vector<vector<int>> ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
             int j=i+1;
             int k=n-1;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             while(j<k){
-                int total=nums[i]+nums[j]+nums[k];
-                if(total>0)
+                
+                int target=nums[i]+nums[j]+nums[k];
+                if(target>0)
                     k--;
-                else if(total<0)
+                else if(target<0)
                     j++;
                 else{
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
-                    while(j<k && nums[j]==nums[j-1] ) j++;
+                    while(j<k && nums[j]==nums[j-1]) j++;
                 }
             }
         }
