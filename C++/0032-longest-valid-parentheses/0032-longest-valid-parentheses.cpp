2118583089556ecb877cc557c5e596999dec79c4
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-21 14:24:34 +0530

    Time: 6 ms (11.11%), Space: 10.2 MB (94.72%) - LeetHub

--- C++/0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ C++/0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
-        int c=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(')
-                st.push(i);
-            else{
-                st.pop();
-                if(st.empty())
-                    st.push(i);
-                else
-                    c=max(c,i-st.top());
-            }
+        int len=0;
+        int l=0,r=0;
+        for(char c:s){
+            if(c=='(') l++;
+            else r++;
+
+            if(l==r) len=max(len,2*r);
+            else if(r>l) l=r=0;
         }
-        return c;
+        l=r=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='(') l++;
+            else r++;
+
+            if(l==r) len=max(len,2*l);
+            else if(l>r) l=r=0;
+        }
+        return len;
     }
 };
