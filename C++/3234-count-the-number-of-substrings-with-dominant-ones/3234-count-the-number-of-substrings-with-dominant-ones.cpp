389822df754c16bf92d7411a43d96c0e28b3ca83
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-22 15:33:32 +0530

    Time: 1485 ms (22.05%), Space: 18 MB (51.18%) - LeetHub

---
+++ C++/3234-count-the-number-of-substrings-with-dominant-ones/3234-count-the-number-of-substrings-with-dominant-ones.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        vector<int> countones(n,0);
+        countones[0]=(s[0]=='1')?1:0;
+
+        for(int i=1;i<n;i++){
+            countones[i]=countones[i-1]+(s[i]=='1'?1:0);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int ones=countones[j]-(i-1>=0?countones[i-1]:0);
+                int zeros=(j-i+1)-ones;
+
+                if(zeros*zeros>ones){
+                    int wasteindexes=zeros*zeros-ones;
+                    j+=wasteindexes-1;
+                }
+                else if(zeros*zeros==ones){
+                    ans++;
+                }
+                else{
+                    ans++;
+                    int k=sqrt(ones)-zeros;
+                    int next=j+k;
+
+                    if(next>=n){
+                        ans+=n-j-1;
+                        break;
+                    }
+                    else
+                        ans+=k;
+                    
+                    j=next;
+                }
+            }
+        }
+        return ans;
+    }
+};
