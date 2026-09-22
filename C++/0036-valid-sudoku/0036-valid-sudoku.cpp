42ef8d76fc19d7f590805b172bc20374f673697e
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-22 11:56:23 +0530

    Time: 0 ms (100%), Space: 21.5 MB (96.76%) - LeetHub

--- C++/0036-valid-sudoku/0036-valid-sudoku.cpp
+++ C++/0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,40 +1,23 @@
 class Solution {
 private:
-    int rowcheck(int i,int j,int n,vector<vector<char>>& board){
-        for(int x=0;x<n;x++){
-            if(x!=j && (board[i][x]==board[i][j]))
+    bool valid(int i,int j,vector<vector<char>>& board){
+        for(int x=0;x<9;x++){
+            if((x!=i && board[x][j]==board[i][j]) || (x!=j && board[i][x]==board[i][j]))
                 return false;
-        }
-        return true;
-    }
-    int colcheck(int i,int j,int m,vector<vector<char>>& board){
-        for(int x=0;x<m;x++){
-            if(x!=i && (board[x][j]==board[i][j]))
-                return false;
-        }
-        return true;
-    }
-    int gridcheck(int i,int j,vector<vector<char>>& board){
-        int boxr=(i/3)*3;
-        int boxc=(j/3)*3;
-
-        for(int x=boxr;x<boxr+3;x++){
-            for(int y=boxc;y<boxc+3;y++){
-                if((x!=i || y!=j) && board[i][j]==board[x][y])
-                    return false;
-            }
+            
+            int boxr=3*(i/3)+(x/3);
+            int boxc=3*(j/3)+(x%3);
+            if((boxr!=i || boxc!=j) &&  board[boxr][boxc]==board[i][j]) return false;
         }
         return true;
     }
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
-                if(!rowcheck(i,j,n,board) || !colcheck(i,j,m,board) || !gridcheck(i,j,board))
+                if(!valid(i,j,board)) 
                     return false;
             }
         }
