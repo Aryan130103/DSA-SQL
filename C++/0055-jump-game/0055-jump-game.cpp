ccfa712da1e859c5235dde72e71a27eeaadb9e2c
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-23 16:42:43 +0530

    Time: 0 ms (100%), Space: 52.2 MB (82.95%) - LeetHub

---
+++ C++/0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxx=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(maxx<i)
+                return false;
+
+            maxx=max(maxx,i+nums[i]);
+            if(maxx>=nums.size()-1)
+                return true;
+        }
+        return true;
+    }
+};
