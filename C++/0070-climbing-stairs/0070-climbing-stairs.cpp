9ad308dea4d0f479ef5a571d00e13a9bfcf053d5
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-23 18:21:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (52.71%) - LeetHub

--- C++/0070-climbing-stairs/0070-climbing-stairs.cpp
+++ C++/0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 private:
-    int f(int n,unordered_map<int ,int>& dp){
+    int f(int n,unordered_map<int,int>& dp){
         if(n==0 || n==1) return 1;
+        
         if(dp.find(n)==dp.end())
             dp[n]=f(n-1,dp)+f(n-2,dp);
-        
         return dp[n];
     }
 public:
     int climbStairs(int n) {
-        unordered_map<int ,int> dp;
+        unordered_map<int,int>dp;
         return f(n,dp);
     }
 };
