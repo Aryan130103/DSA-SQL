c1d181f3eb88ba9a1926c584156cf95f7abacd63
Aryan Raj <118997490+Aryan130103@users.noreply.github.com>
2026-09-24 15:37:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (78.83%) - LeetHub

--- C++/0091-decode-ways/0091-decode-ways.cpp
+++ C++/0091-decode-ways/0091-decode-ways.cpp
@@ -3,22 +3,21 @@ public:
     int numDecodings(string s) {
         int n=s.size();
         if(s[0]=='0') return 0;
+        vector<int> dp(n+1,0);
 
-        int prev1=1,prev2=1;
+        dp[0]=1;
+        dp[1]=1;
 
         for(int i=2;i<=n;i++){
-            int onedigit=s[i-1]-'0';
-            int twodigit=(s[i-2]-'0')*10+onedigit;
+            int onedigits=s[i-1]-'0';
+            int twodigits=(s[i-2]-'0')*10+onedigits;
 
-            int curr=0;
-
-            if(onedigit>=1)
-                curr+=prev1;
-            if(twodigit>=10 && twodigit<=26)
-                curr+=prev2;
-            prev2=prev1;
-            prev1=curr;
+            if(onedigits>=1 && onedigits<=9)
+                dp[i]+=dp[i-1];
+            if(twodigits>=10 && twodigits<=26)
+                dp[i]+=dp[i-2];
+            
         }
-        return prev1;
+        return dp[n];
     }
 };
